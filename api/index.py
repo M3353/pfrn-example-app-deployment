741576a0836ee712e94448a559164dac2a84ff72
Jack Li <li.jack98@gmail.com>
2023-03-09 15:35:44 -0500

    deployment

--- api/index.py
+++ api/index.py
@@ -1,1 +1,10 @@
-from wsgi import app
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from api import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
